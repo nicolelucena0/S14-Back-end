24bb7737b6305e1fff177d61596e90cc3a74b27f
Nicole Cardoso <00001106178968sp@aluno.educacao.sp.gov.br>
2025-06-24 19:32:41 -0300

    crie test

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from pymongo import MongoClient
 
 mongo_uri = "mongodb+srv://nick37:17092007Nc@cluster0.ftznygf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongo_client = MongoClient(mongo_uri)
+
+database = mongo_client['database_test']
+test_collection = database['test_collection']
+
+person = {"name"; "Lucca de Enzo", "age": 12 }
+test_collection.insert_one(person)
