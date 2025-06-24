526af699f952caa014935a66b9fc41468cf9ab17
Nicole Cardoso <00001106178968sp@aluno.educacao.sp.gov.br>
2025-06-24 19:26:34 -0300

    oie

---
+++ main.py
@@ -0,0 +1,4 @@
+from pymongo import MongoClient
+
+mongo_uri = "mongodb+srv://nick37:17092007Nc@cluster0.ftznygf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongo_client = MongoClient(mongo_uri)
